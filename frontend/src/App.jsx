9a479d5074967a156f87b5bb1906c1b6d1916947
Your Name <you@example.com>
2023-10-21 16:48:26 +0530

    create design completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Index from './pages/Index'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Index from "./pages/Index";
 import Layout from "./pages/Layout";
-import Home from './components/Home'
-import Projects from './components/Projects'
-import Templates from './components/Templates'
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import Templates from "./components/Templates";
+import CreateDesign from "./components/CreateDesign";
 
 const router = createBrowserRouter([
   {
@@ -14,23 +12,27 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       {
-        path: '/',
-        element: <Home />
+        path: "/",
+        element: <Home />,
       },
       {
-        path: '/templates',
-        element: <Templates />
+        path: "/templates",
+        element: <Templates />,
       },
       {
-        path: '/projects',
-        element: <Projects />
-      }
-    ]
-  }
-])
+        path: "/projects",
+        element: <Projects />,
+      },
+    ],
+  },
+  {
+    path: "/design/create",
+    element: <CreateDesign />,
+  },
+]);
 
 function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
